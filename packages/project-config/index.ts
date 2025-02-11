81b390cdc64b9b57f8cc67e951cc9949171b5976
batuhanbilginn <batuhanbilginn@gmail.com>
2025-02-11 22:31:30 +0300

    wip:project setup

---
+++ packages/project-config/index.ts
@@ -0,0 +1,13 @@
+export const projectConfig = {
+	name: "poe",
+	description: "AI-powered content suite for tech-savvy marketing teams.",
+	/* Contact */
+	email: {
+		support: "support@usepoe.com",
+		sales: "sales@usepoe.com",
+	},
+	links: {
+		marketing: "https://usepoe.com",
+		app: "https://app.usepoe.com",
+	},
+};
